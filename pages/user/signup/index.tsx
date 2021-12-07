f1c66941ac736df645ff05ba9230541d675c77bb
apartgecko88165 <apartgecko88165@gmail.com>
2021-12-07 18:19:49 +0000

    added better form functionality

--- pages/user/signup/index.tsx
+++ pages/user/signup/index.tsx
@@ -1,15 +1,30 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-// import Gun from "gun";
-
-// CSS that I couldn't figure out how to put in Next.js
+import { useState } from "react";
+import Gun from "gun";
 
 const Main: NextPage = () => {
-  // const gun = Gun();
+  const gun = Gun();
 
   const registerUser = async (event: any) => {
     event.preventDefault();
     console.log("event")
+  }
+
+  var [uname, setUname] = useState("");
+  var [userPassword, setUserPassword] = useState("");
+  var [confirmation, setConfirmation] = useState("");
+
+  function handleUname(event: any) {
+    setUname(event.target.value);
+  }
+
+  function handlePassword(event: any) {
+    setUserPassword(event.target.value);
+  }
+
+  function handleConfirmation(event: any) {
+    setConfirmation(event.target.value);
   }
 
   return (
@@ -21,11 +36,11 @@ const Main: NextPage = () => {
     />
       <form onSubmit={registerUser} className="formBody">
         <label htmlFor="uname" className="newline">Username</label>
-        <input name="uname" type="text" className="inputField" placeholder="Minimum 6 characters" />
+        <input name="uname" type="text" className="inputField" placeholder="Minimum 6 characters" onChange={handleUname} />
         <label htmlFor="passwrd" className="newline">Password</label>
-        <input name="passwrd" type="password" className="inputField" placeholder="Good: 1b&bnf?+_YhRE$" />
+        <input name="passwrd" type="password" className="inputField" placeholder="Good: 1b&bnf?+_YhRE$" onChange={handlePassword} />
         <label htmlFor="re-enter" className="newline">Re-enter password</label>
-        <input name="re-enter" type="password" className="inputField" />
+        <input name="re-enter" type="password" className="inputField" onChange={handleConfirmation} />
         <ul style={{
           display: "flex",
           flexDirection: "row-reverse",
@@ -33,7 +48,11 @@ const Main: NextPage = () => {
           margin: "2.5rem",
         }}>
           <li className="navItem select">Sign in</li>
-          <li className="navItem select" style={{ left: "0%" }}>Register</li>
+          <li className="navItem select" style={{ left: "0%" }} onClick={() => {
+            if (userPassword === confirmation) {
+              gun.get("user")
+            }
+          }}>Register</li>
         </ul>
       </form>
     </div>
